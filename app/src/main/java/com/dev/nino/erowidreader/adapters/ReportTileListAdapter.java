c72f958a34c0f27e1fd36fe5ad1347f63142416c
Nino <ninojansen5@gmail.com>
2018-04-26 00:15:23 +0200

    Implemented initial version of database with substances and experiencetypes support

--- app/src/main/java/com/dev/nino/erowidreader/adapters/ReportTileListAdapter.java
+++ app/src/main/java/com/dev/nino/erowidreader/adapters/ReportTileListAdapter.java
@@ -53,6 +53,7 @@ public class ReportTileListAdapter extends RecyclerView.Adapter<RecyclerView.Vie
                             onLoadMoreListener.onLoadMore();
                         }
                     }
+
                 }
             });
         }
